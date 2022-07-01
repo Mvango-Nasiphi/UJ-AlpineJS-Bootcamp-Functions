bcf1cd3a255684167191f8d8d85f1e16742f9804
Nasiphi <mvangonasiphi17@gmail.com>
2022-07-01 17:45:48 +0200

    initial commit

---
+++ greet-widget.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('greetWidget', function(){
+        return {
+            name: '',
+            message: '',
+            greetMe() {
+
+                this.message = greet(this.name);
+                setTimeout(() => {
+                    this.message = '';
+                }, 3000);
+            }
+        }
+    });
+})
---
+++ transport-fee-widget.js
@@ -0,0 +1,13 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('transportFeeWidget', function () {
+        return {
+            workingShift: '',
+            transportFeeMessage: '',
+            calculate() {
+                const message = transportFee(this.workingShift);
+                this.transportFeeMessage = message;
+            }
+        }
+    });
+})
+
